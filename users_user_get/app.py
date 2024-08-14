00bc0cf0e4cd9df80e6614aedff94a5a3aece759
Apinheiro78 <57006070+Apinheiro78@users.noreply.github.com>
2024-08-14 09:29:34 -0300

    FIX: Cambio user get

--- users_user_get/app.py
+++ users_user_get/app.py
@@ -1,8 +1,14 @@
 import json
 import postgre
+import logging
+
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
 
 def lambda_handler(event, context):
     try:
+        logger.info(event)
+        
         username = event['queryStringParameters']['username']
         password = event['queryStringParameters']['password']
 
@@ -10,10 +16,19 @@ def lambda_handler(event, context):
         results = postgre.query_postgresql(query)
         
         return {
-            'statusCode': 200,
-            'body': json.dumps(results)
+            "statusCode": 200,
+            "headers": {
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Headers": "Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token",
+                "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
+            },
+            "body": json.dumps(results)
         }
-    except:
+        
+    except Exception as e:
+        # Registrar el error en el logger
+        logger.error("Ocurrió un error: %s", str(e), exc_info=True)
+        
         return {
             'statusCode': 500
         } 
