ca526c99ba2e60450596840c2fe23005268d7463
martinferreiraprovenzano <martinferreiraprovenzano@gmail.com>
2024-09-18 12:59:30 -0300

    correction bugs

--- pathologies_symptoms_get_pending/app.py
+++ pathologies_symptoms_get_pending/app.py
@@ -3,11 +3,11 @@ import postgre
 
 def lambda_handler(event, context):
 
-    query = "SELECT pathologies.pat_id, pathologies.name AS nombre_patologia, symptoms.sym_id, symptoms.name AS nombre_sintoma FROM pathologies_symptoms JOIN pathologies ON pathologies_symptoms.pat_id = pathologies.pat_id JOIN symptoms ON pathologies_symptoms.sym_id = symptoms.sym_id WHERE pathologies_symptoms.state = 'pendiente'"
+    query = "SELECT pathologies.pat_id, pathologies.name AS nombre_patologia, symptoms.sym_id, symptoms.name AS nombre_sintoma, pathologies_symptoms.username FROM pathologies_symptoms JOIN pathologies ON pathologies_symptoms.pat_id = pathologies.pat_id JOIN symptoms ON pathologies_symptoms.sym_id = symptoms.sym_id WHERE pathologies_symptoms.state = 'pendiente'"
 
     results = postgre.query_postgresql(query)
     
-    output = [{"pat_id": row[0],"pat_name": row[1],"sym_id": row[2],"sym_name": row[3]} for row in results]
+    output = [{"pat_id": row[0],"pat_name": row[1],"sym_id": row[2],"sym_name": row[3],"username": row[4]} for row in results]
     
     return {
         "statusCode": 200,
