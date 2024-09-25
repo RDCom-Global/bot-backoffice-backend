aace3b70b43856381e181199af1845896ba094b4
martinferreiraprovenzano <martinferreiraprovenzano@gmail.com>
2024-09-25 10:47:08 -0300

    correction bugs

---
+++ symptoms_validate_second_ok/app.py
@@ -0,0 +1,20 @@
+import json
+import postgre
+
+def lambda_handler(event, context):
+
+    id = event['queryStringParameters']['sym_id']
+
+    query = "update symptoms set state = 'segunda verificación ok' where sym_id = '"+ id +"'"
+
+    results = postgre.insert_postgresql(query)
+    
+    return {
+        "statusCode": 200,
+        "headers": {
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Headers": "Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token",
+                "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
+                   },
+        "body": json.dumps(results)
+    }
