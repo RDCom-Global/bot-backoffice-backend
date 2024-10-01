a9d93ccc694ba0829934000c9d6f60f2555141e5
martinferreiraprovenzano <martinferreiraprovenzano@gmail.com>
2024-10-01 12:02:21 -0300

    agregado lamdas para sintomas sin categoria

---
+++ symptoms_get_all_symptoms_category_pending/app.py
@@ -0,0 +1,18 @@
+import json
+import postgre
+
+def lambda_handler(event, context):
+    query = "SELECT * FROM symptoms s LEFT JOIN category_symptoms cs ON s.sym_id = cs.sym_id WHERE cs.sym_id IS NULL"
+    results = postgre.query_postgresql(query)
+    
+    output = [{"sym_id": row[0],"name": row[1],"synonymous": row[2],"state": row[3],"link": row[4],"hpo_id": row[5],"username": row[6]} for row in results]
+   
+    return {
+        "statusCode": 200,
+        "headers": {
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Headers": "Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token",
+                "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
+                   },
+        "body": json.dumps(output)
+    }
