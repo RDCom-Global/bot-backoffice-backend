fe86557ad146ac818293b7470aa45e3797011bf7
Apinheiro78 <57006070+Apinheiro78@users.noreply.github.com>
2024-10-01 16:05:49 -0300

    Fix de consultas sobre sym category pending

--- symptoms_get_all_symptoms_category_pending_count/app.py
+++ symptoms_get_all_symptoms_category_pending_count/app.py
@@ -2,7 +2,7 @@ import json
 import postgre
 
 def lambda_handler(event, context):
-    query = "SELECT COUNT(*) FROM symptoms s LEFT JOIN category_symptoms cs ON s.sym_id = cs.sym_id WHERE cs.sym_id IS NULL"
+    query = "SELECT COUNT(*) FROM symptoms s LEFT JOIN categories_symptoms cs ON s.sym_id = cs.sym_id WHERE cs.sym_id IS NULL"
     results = postgre.query_postgresql(query)
     
     output = [{"sym_id": row[0],"name": row[1],"synonymous": row[2],"state": row[3],"link": row[4],"hpo_id": row[5],"username": row[6]} for row in results]
